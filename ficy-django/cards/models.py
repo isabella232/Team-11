41f7c1eb5a34d3803ff0afa82aad09c183233437
sebvil <sebasvime@gmail.com>
2019-10-12 08:22:20 -0500

    Added card models

--- ficy-django/cards/models.py
+++ ficy-django/cards/models.py
@@ -2,3 +2,7 @@ from django.db import models
 
 
 # Create your models here.
+class Card(models.Model):
+    title = models.CharField(max_length=100, primary_key=True)
+    description = models.TextField()
+    image = models.FilePathField(path="/img")
