dd1c6abfb379994f24963f788359f45b7eed04b5
Marco Anaya <marco.a.anaya34@gmail.com>
2019-10-12 09:39:25 -0500

    added views

--- ficy-django/cards/views.py
+++ ficy-django/cards/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from cards.models import Card
+
+def card_index(request):
+    cards = Card.objects.all()
+    context = {
+        'cards': cards
+    }
+    return render(request, 'index_html', context)
+
+def card_detail(request):
+    card = Card.objects
+    context = {
+        'card': card
+    }
+    return render(request, 'children.html', context)
+
 
 # Create your views here.
